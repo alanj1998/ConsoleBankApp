87db234774017c979435e421386022946e88f59c
Alan <ali9427@windowslive.com>
2019-10-23 22:33:22 +0100

    fixes found in broken:

--- Models/BankAdmin.cs
+++ Models/BankAdmin.cs
@@ -4,9 +4,6 @@ namespace SSD.Models
 {
     public class BankAdmin : Person
     {
-
-        public string ManagerId { get; set; }
-        public BankAdmin Manager { get; set; }
         public string BranchLocation { get; set; }
         public BankAdmin() { }
     }
